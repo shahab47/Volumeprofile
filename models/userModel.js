fccbb2ea8c7dc06b0979932b89e4ace38494a52b
shahab68 <csshahab47@gmail.com>
2023-12-26 10:07:40 +0330

    create models

---
+++ models/userModel.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+const userSchema = mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "Must Provide a username."],
+      unique: [true, "Must be unique"],
+    },
+    email: {
+      type: String,
+      required: [true, "Must Provide an email."],
+      unique: [true, "Must be unique."],
+    },
+    password: {
+      type: String,
+      required: [true, "Must provide a password."],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+const User = mongoose.models.User || mongoose.model("User", userSchema);
+export default User;
