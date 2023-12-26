4f8968353278089c54eb7217048a46768f6a7151
shahab68 <csshahab47@gmail.com>
2023-12-26 15:50:07 +0330

    Add register page

--- app/page.tsx
+++ app/page.tsx
@@ -25,24 +25,5 @@ const Home = ()=>{
 }
 export default Home;
 
-"use client"
 
-import React {useState} from 'react';
-
-const Home =()=>{
-  const [count,setCount]=useState(0);
-  const increament =()=>{
-    setCount(count+1);
-  }
-  const decrement =()=>{
-    setCount(count-1);
-  }
-  return(
-    <div>
-      <label>{count}</label>
-      <button onClick={increament}>+1</button>
-      <button onClick={decrement}>-1</button>
-    </div>
-    )
-}
 
---
+++ app/register/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import LoginAndRegiester from '../components/loginRegister/LoginAndRegiester'
+
+function page() {
+  return (
+    <div>
+        <LoginAndRegiester/>
+    </div>
+  )
+}
+
+export default page
