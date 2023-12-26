86a677e76cc2151c5e50e29642462713c1e38289
shahab68 <csshahab47@gmail.com>
2023-12-26 09:56:53 +0330

    Create utils install mongoose

---
+++ utils/connect.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+export async function connectDB() {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected To DB.");
+  } catch (error) {
+    console.log("Error While Connecting", error);
+  }
+}
