1998fd947a224630c8222c4d6e0c2fd030d0b6a2
shahab68 <csshahab47@gmail.com>
2024-01-27 13:55:39 +0330

    ADD EYE ICON

--- app/components/Register/Register.js
+++ app/components/Register/Register.js
@@ -1,5 +1,9 @@
 "use client";
 import React, { useState } from "react";
+import { FaEye } from "react-icons/fa6";
+//<FaEye />
+import { FaEyeSlash } from "react-icons/fa6";
+//<FaEyeSlash />
 
 const Register = () => {
   const [info, setInfo] = useState({
@@ -12,11 +16,22 @@ const Register = () => {
   const [error, setError] = useState("");
   const [pending, setPending] = useState(false);
 
+  const [passwordVisible, setPasswordVisible] = useState(false);
+
+  const handlePasswordVisibilityToggle = () => {
+    setPasswordVisible(!passwordVisible);
+    console.log(passwordVisible);
+  };
+
   function handleInput(e) {
     setInfo((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+
+    if(e.target.name == "confpassword" && e.target.value != info.password){
+      console.log("Passwords do not match.")
+    }
     //console.log(info);
-    //console.log(e.target.name);
-    //console.log(e.target.value);
+    console.log(e.target.name);
+    console.log(e.target.value);
   }
 
   async function handleSubmit(e) {
@@ -25,12 +40,15 @@ const Register = () => {
     
     if (!info.username || !info.email || !info.password || !info.confpassword) {
       setError("Must Provide all the Credential.");
+      return;
     }
     if (info.confpassword != info.password) {
       setError("Password is not Match.");
+      return;
     }
     if (info.terms == "False"){
       setError("Please Checked Terms And Conditions")
+      return;
     }
     try {
       setPending(true);
@@ -90,6 +108,7 @@ const Register = () => {
                     UserName
                   </label>
                   <input
+                    id="username"
                     type="text"
                     name="username"
                     className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
@@ -123,24 +142,25 @@ const Register = () => {
                     Password
                   </label>
                   <input
-                    type="password"
+                    type={passwordVisible ? "text" : "password"}
                     name="password"
                     id="password"
                     placeholder="••••••••"
                     className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     required
                     onChange={(e) => handleInput(e)}
-                  />
+                    
+                  /><div onClick={handlePasswordVisibilityToggle}>{passwordVisible ? <FaEye /> : <FaEyeSlash />}</div>
                 </div>
                 <div>
                   <label
-                    htmlFor="confpassword"
+                    htmlFor="confirm-password"
                     className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                   >
                     Confirm password
                   </label>
                   <input
-                    type="password"
+                    type={passwordVisible ? "text" : "password"}
                     name="confpassword"
                     id="confirm-password"
                     placeholder="••••••••"
@@ -153,6 +173,7 @@ const Register = () => {
                   <div className="flex items-center h-5">
                     <input
                       name="terms"
+                      id="terms"
                       aria-describedby="terms"
                       type="checkbox"
                       className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
